05eb58a263375d1bb3dde741eef532822a9ed963
Nirav Parekh <niruparekh09@gmail.com>
2022-07-22 13:58:05 +0530

    completed the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,28 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-
+import { ThemeProvider } from 'styled-components';
 import Home from './pages/Home';
 import Starred from './pages/Starred';
 import Show from './pages/Show';
 
+const theme = {
+  mainColors: {
+    blue: '#2400ff',
+    gray: '#c6c6c6',
+    dark: '#353535',
+  },
+};
+
 function App() {
   return (
-    <Routes>
-      <Route exact path="/" element={<Home />} />
-      <Route exact path="/starred" element={<Starred />} />
-      <Route exact path="/show/:id" element={<Show />} />
-      <Route element={<div>Not Found</div>} />
-    </Routes>
+    <ThemeProvider theme={theme}>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/starred" element={<Starred />} />
+        <Route exact path="/show/:id" element={<Show />} />
+        <Route element={<div>Not Found</div>} />
+      </Routes>
+    </ThemeProvider>
   );
 }
 
